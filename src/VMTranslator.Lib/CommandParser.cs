d4171356b11c50fb656384ab7ddf7be99fa3fbf2
propes <>
2020-04-25 11:08:13 +1000

    refactor command parser

--- src/VMTranslator.Lib/CommandParser.cs
+++ src/VMTranslator.Lib/CommandParser.cs
@@ -5,46 +5,38 @@ namespace VMTranslator.Lib
 {
     public class CommandParser : ICommandParser
     {
-        private readonly ICommandParser arithmeticCommandParser;
-        private readonly ICommandParser stackOperationCommandParser;
-
-        private readonly Dictionary<string, CommandType> commandTypes =
-            new Dictionary<string, CommandType>
-            {
-                { "push", CommandType.StackOperation },
-                { "pop", CommandType.StackOperation },
-                { "add", CommandType.Arithmetic },
-                { "sub", CommandType.Arithmetic },
-                { "neg", CommandType.Arithmetic },
-                { "eq", CommandType.Arithmetic },
-                { "gt", CommandType.Arithmetic },
-                { "lt", CommandType.Arithmetic },
-                { "and", CommandType.Arithmetic },
-                { "or", CommandType.Arithmetic },
-                { "not", CommandType.Arithmetic }
-            };
+        private readonly Dictionary<string, ICommandParser> parsers;
 
         public CommandParser(
             ICommandParser arithmeticCommandParser,
             ICommandParser stackOperationCommandParser)
         {
-            this.arithmeticCommandParser = arithmeticCommandParser;
-            this.stackOperationCommandParser = stackOperationCommandParser;
+            parsers = new Dictionary<string, ICommandParser>
+            {
+                { "push", stackOperationCommandParser },
+                { "pop", stackOperationCommandParser },
+                { "add", arithmeticCommandParser },
+                { "sub", arithmeticCommandParser },
+                { "neg", arithmeticCommandParser },
+                { "eq", arithmeticCommandParser },
+                { "gt", arithmeticCommandParser },
+                { "lt", arithmeticCommandParser },
+                { "and", arithmeticCommandParser },
+                { "or", arithmeticCommandParser },
+                { "not", arithmeticCommandParser }
+            };
         }
 
         public ICommand Parse(string line, string staticVariableName = null)
         {
             var firstToken = line.Split(' ')[0];
 
-            if (!commandTypes.ContainsKey(firstToken))
+            if (!parsers.ContainsKey(firstToken))
             {
                 throw new InvalidOperationException($"{firstToken} is not a recognised command");
             }
 
-            if (commandTypes[firstToken] == CommandType.Arithmetic)
-                return arithmeticCommandParser.Parse(line, staticVariableName);
-
-            return stackOperationCommandParser.Parse(line, staticVariableName);
+            return parsers[firstToken].Parse(line, staticVariableName);
         }
     }
 }
