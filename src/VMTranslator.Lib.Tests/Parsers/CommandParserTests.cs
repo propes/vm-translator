46b093f2e76e3cba749107f46f259da3a2b35476
propes <>
2020-04-29 11:07:17 +1000

    add unit tests for command parser

---
+++ src/VMTranslator.Lib.Tests/Parsers/CommandParserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+
+namespace VMTranslator.Lib.Tests
+{
+    public class CommandParserTests
+    {        
+        [Theory]
+        [InlineData("")]
+        [InlineData("pop")]
+        [InlineData("pop static")]
+        public void Parse_GivenInvalidCommand_ThrowsException(string command)
+        {
+            var sut = new CommandParser();
+
+            Assert.ThrowsAny<InvalidOperationException>(() => sut.Parse(command));
+        }
+
+        [Fact]
+        public void Parse_GivenValidCommand_CapturesKeyword()
+        {
+            var actual = new CommandParser().Parse("pop static 2");
+
+            Assert.Equal("pop", actual.Keyword);
+        }
+        
+        [Fact]
+        public void Parse_GivenValidCommand_CapturesSegment()
+        {
+            var actual = new CommandParser().Parse("pop static 2");
+
+            Assert.Equal("static", actual.Segment);
+        }
+
+        [Fact]
+        public void Parse_GivenValidCommand_CapturesIndex()
+        {
+            var actual = new CommandParser().Parse("pop static 2");
+
+            Assert.Equal("2", actual.Index);
+        }
+    }
+}
--- src/VMTranslator.Lib/Parsers/CommandParser.cs
+++ src/VMTranslator.Lib/Parsers/CommandParser.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace VMTranslator.Lib
 {
     public class CommandParser : ICommandParser
@@ -5,6 +7,11 @@ namespace VMTranslator.Lib
         public Command Parse(string line)
         {
             var parts = line.Split(' ');
+
+            if (parts.Length < 3)
+            {
+                throw new InvalidOperationException("Command must be in the format 'keyword segment index'");
+            }
 
             return new Command(parts[0], parts[1], parts[2]);
         }
