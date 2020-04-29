99ba2fce8aa0f01e66b5f5456052251b4265423c
propes <>
2020-04-29 10:43:17 +1000

    add unit tests for counter

---
+++ src/VMTranslator.Lib.Tests/Counters/CounterTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace VMTranslator.Lib.Tests
+{
+    public class CounterTests
+    {
+        [Fact]
+        public void Count_InitialCountIsZero()
+        {
+            var actual = new Counter().Count;
+
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Increment_IncrementsCountToOne()
+        {
+            var counter = new Counter();
+            counter.Increment();
+
+            Assert.Equal(1, counter.Count);
+        }
+
+        [Fact]
+        public void Increment_IncrementsCountToTwo()
+        {
+            var counter = new Counter();
+            counter.Increment();
+            counter.Increment();
+
+            Assert.Equal(2, counter.Count);
+        }
+    }
+}
