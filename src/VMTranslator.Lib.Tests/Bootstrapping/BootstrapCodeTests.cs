f5407ccb859cac29f63432d8f7b1c11a0c10010c
propes <>
2020-05-10 11:00:34 +1000

    add bootstrap code

---
+++ src/VMTranslator.Lib.Tests/Bootstrapping/BootstrapCodeTests.cs
@@ -0,0 +1,85 @@
+﻿using Xunit;
+
+namespace VMTranslator.Lib.Tests
+{
+    public class BootstrapCodeTests
+    {
+        [Fact]
+        public void Name()
+        {
+            var expected = new []
+            {
+                "// SP=256",
+                "@256",
+                "D=A",
+                "@SP",
+                "M=D",
+                "",
+                $"// call Sys.init 0",
+                $"// push Sys.init$ret.0",
+                $"@Sys.init$ret.0",
+                "D=A",
+                "@SP",
+                "A=M",
+                "M=D",
+                "@SP",
+                "M=M+1",
+                "// push LCL",
+                "@LCL",
+                "D=M",
+                "@SP",
+                "A=M",
+                "M=D",
+                "@SP",
+                "M=M+1",
+                "// push ARG",
+                "@ARG",
+                "D=M",
+                "@SP",
+                "A=M",
+                "M=D",
+                "@SP",
+                "M=M+1",
+                "// push THIS",
+                "@THIS",
+                "D=M",
+                "@SP",
+                "A=M",
+                "M=D",
+                "@SP",
+                "M=M+1",
+                "// push THAT",
+                "@THAT",
+                "D=M",
+                "@SP",
+                "A=M",
+                "M=D",
+                "@SP",
+                "M=M+1",
+                "// ARG = SP-5-nArgs",
+                "@SP",
+                "D=M",
+                "@5",
+                "D=D-A",
+                $"@0",
+                "D=D-A",
+                "@ARG",
+                "M=D",
+                "// LCL = SP",
+                "@SP",
+                "D=M",
+                "@LCL",
+                "M=D",
+                $"// goto Sys.init",
+                $"@Sys.init",
+                "0;JMP",
+                $"(Sys.init$ret.0)",
+                ""
+            };
+
+            var actual = new BootstrapCode().ToAssembly();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/VMTranslator.Lib/Bootstrapping/BootstrapCode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace VMTranslator.Lib
+{
+    public class BootstrapCode
+    {
+        public IEnumerable<string> ToAssembly()
+        {
+            var assemblyLines = new List<string>();
+            assemblyLines.AddRange(new []
+            {
+                "// SP=256",
+                "@256",
+                "D=A",
+                "@SP",
+                "M=D",
+                ""
+            });
+            assemblyLines
+                .AddRange(new CallFunctionTranslator(new FunctionCallCounter())
+                    .ToAssembly("call Sys.init 0"));
+
+            return assemblyLines;
+        }
+    }
+}
